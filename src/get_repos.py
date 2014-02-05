855b9e2de3a3d1fed2f3d55fd6219b57b9d3fffe
Abe Kim <abekim0607@gmail.com>
2014-02-05 16:01:55 -0500

    working to implement a student model

--- src/get_repos.py
+++ src/get_repos.py
@@ -2,98 +2,122 @@ from git import Repo
 import os, shutil
 
 def test_urls (users, url="https://github.com/%s/SoftwareDesign"):
-	"""
-	Test whether or not the urls are valid
-	"""
-	import urllib2
+  """
+  Test whether or not the urls are valid
+  """
+  import urllib2
 
-	failed = [] # keep track of failed user ids
-	
-	for uid in users:
-		try:
-			urllib2.urlopen(url % uid)
-		except:
-			failed.append(uid)
-			print uid + " has failed to load"
-	
-	print "Loaded %i of %i successfully" % (len(users) - len(failed), len(users))
-	print "Failed user ids: %s" % failed
+  failed = [] # keep track of failed user ids
+  
+  for uid in users:
+    try:
+      urllib2.urlopen(url % uid)
+    except:
+      failed.append(uid)
+      print uid + " has failed to load"
+  
+  print "Loaded %i of %i successfully" % (len(users) - len(failed), len(users))
+  print "Failed user ids: %s" % failed
 
 # unnecessary since json-ing the usernames.
 # def get_username(url):
-# 	"""
-# 	Extracts the username of the student from the github url
-# 	"""
-# 	return url[url.find("com/") + 4:url.find("/SoftwareDesign")]
+#   """
+#   Extracts the username of the student from the github url
+#   """
+#   return url[url.find("com/") + 4:url.find("/SoftwareDesign")]
 
 
 def clone_repos(users, syspath="../repos/", url="https://github.com/%s/SoftwareDesign"):
-	"""
-	Recursively removes previous copies of the repo (requires user confirmation)
-	Clones the repos from the urls to a folder called repos/<username>
+  """
+  Recursively removes previous copies of the repo (requires user confirmation)
+  Clones the repos from the urls to a folder called repos/<username>
 
-		users : list of github ids
-		syspath : system path to copy repos to
-	"""
-	if (raw_input("Remove current repositories? (y/n) ")) != "y":
-		raise Exception("Failed to confirm. Failed to clone repos")
-	
-	# if other repos exist, remove them
-	if os.path.exists(syspath):
-		shutil.rmtree(syspath) # remove existing repos
-		print "Successfully removed repos from \"%s\"" % syspath
+    users : list of github ids
+    syspath : system path to copy repos to
+  """
+  if (raw_input("Remove current repositories? (y/n) ")) != "y":
+    raise Exception("Failed to confirm. Failed to clone repos")
+  
+  # if other repos exist, remove them
+  if os.path.exists(syspath):
+    shutil.rmtree(syspath) # remove existing repos
+    print "Successfully removed repos from \"%s\"" % syspath
 
-	for uid in users:
-		path = syspath + uid
-		repo_url = url + ".git"
-		
-		print "Cloning Repo: %s to %s" % (uid, path)
+  for uid in users:
+    path = syspath + uid
+    repo_url = url + ".git"
+    
+    print "Cloning Repo: %s to %s" % (uid, path)
 
-		if not os.path.exists(path):
-			os.makedirs(path)
-		
-		Repo.clone_from(url % uid, path)
+    if not os.path.exists(path):
+      os.makedirs(path)
+    
+    Repo.clone_from(url % uid, path)
 
-	print "Successfully cloned repos"
+  print "Successfully cloned repos"
 
 def pull_repos(syspath="../repos/"):
-	"""
-	Pulls from remote for all directories under syspath
-	"""
-	for fold in os.listdir(syspath):
-		print "Pulling Repo for ", fold
-		repo = Repo(os.path.join(syspath,fold))
+  """
+  Pulls from remote for all directories under syspath
+  """
+  for fold in os.listdir(syspath):
+    print "Pulling Repo for ", fold
+    repo = Repo(os.path.join(syspath,fold))
 
-		o = repo.remotes.origin
-		o.pull()
+    o = repo.remotes.origin
+    o.pull()
 
 if __name__ == "__main__":
-	# all users
-	users =["acejang1994", "aloverso", "AmandaSutherland", "atproofer", "bishiguro", "cbauerswald", "cebeery", "cwallac", "daouani", "dcelik", "ddiggins", "dennis-chen", "dimitdim", "dinopants174", "doyunglee", "eengel", "emocallaghan", "flymperopoulos", "gabriellee", "gregcole", "griffint", "gubbatuba", "hpelletier", "HWilk", "hzhugit", "iangmhill", "InseongJoe", "jabb1123", "jagreene", "JenniferLVaccaro", "jenwei", "jmorris1993", "jsapers", "jwoo1123", "ksuzy31", "kyflores", "logandavis", "lvanderlyn", "mafaldaborges", "maorbernstein", "MJAFort", "mortier", "ndhanushkodi", "pencilEraser", "PMKeene", "Pratool", "ptitchener", "RRameshwar", "runnersaw", "rvanderheyde", "segerphilip", "SeongHyeok", "sgrim3", "shrinidhit", "srli", "ssingal05", "swalters4925", "themythicaldrago", "yunhsincynthiachen"]
-	print "Current Number of repos: ", len(users)
+  # all users
+  users =["acejang1994", "aloverso", "AmandaSutherland", "atproofer", "bishiguro", "cbauerswald", "cebeery", "cwallac", "daouani", "dcelik", "ddiggins", "dennis-chen", "dimitdim", "dinopants174", "doyunglee", "eengel", "emocallaghan", "flymperopoulos", "gabriellee", "gregcole", "griffint", "gubbatuba", "hpelletier", "HWilk", "hzhugit", "iangmhill", "InseongJoe", "jabb1123", "jagreene", "JenniferLVaccaro", "jenwei", "jmorris1993", "jsapers", "jwoo1123", "ksuzy31", "kyflores", "logandavis", "lvanderlyn", "mafaldaborges", "maorbernstein", "MJAFort", "mortier", "ndhanushkodi", "pencilEraser", "PMKeene", "Pratool", "ptitchener", "RRameshwar", "runnersaw", "rvanderheyde", "segerphilip", "SeongHyeok", "sgrim3", "shrinidhit", "srli", "ssingal05", "swalters4925", "themythicaldrago", "yunhsincynthiachen"]
+  print "Current Number of repos: ", len(users)
 
-	# test_urls(users)
-	# clone_repos(users)
+  # test_urls(users)
+  # clone_repos(users)
 
-	from names import family
+  import csv
 
-	matches = []
-	nonMatches = []
+  students = {}
 
-	matchCount = 0
-	nonMatchCount = 0
+  with open('mystudents.csv', 'rb') as csvfile:
+    read = csv.reader(csvfile)
 
-	for n in family:
-		uid = n.split()[-1]
-		if uid in users:
-			matches.append(uid)
-			matchCount += 1
-			users.remove(uid)
-		else:
-			nonMatches.append(uid)
-			nonMatchCount += 1
+    for row in read:
+      uid = row[0]
+      name = row[1]
+      email = row[2]
 
-	print "Number matched: ", matchCount
-	print "Number unmatched: ", nonMatchCount
-	print "nonMatches: ", nonMatches
-	print "students who haven't forked: ", users
+      if not(len(uid)):
+        print "User ID empty! ID:",name
+      else:
+        students[uid] = {"name": name, "email": email, "user": uid}
+
+
+  import json
+
+  with open('mystudents.json', 'wb') as f:
+    json.dump(students, f)
+
+
+  from names import family
+
+  matches = []
+  nonMatches = []
+
+  matchCount = 0
+  nonMatchCount = 0
+
+  for n in family:
+    uid = n.split()[-1]
+    if uid in users:
+      matches.append(uid)
+      matchCount += 1
+      users.remove(uid)
+    else:
+      nonMatches.append(uid)
+      nonMatchCount += 1
+
+  print "Number matched: ", matchCount
+  print "Number unmatched: ", nonMatchCount
+  print "nonMatches: ", nonMatches
+  print "students who haven't forked: ", users
---
+++ src/models/students.py
@@ -0,0 +1,23 @@
+class Student (object):
+  def __init__(self, name="", email="", user=""): #, **kwargs):
+    self.indexes = []
+
+    self.name = name
+    self.email = email
+    self.user = user
+
+    # if kwargs is not None:
+    #   for key, value in kwargs.iteritems():
+    #     self.indexes.append(key)
+    #     self.key = value
+    #     print "%s, %s" % (key, value)
+
+  def __str__(self):
+    return "%s, Github handle: %s" % (self.name, self.user)
+
+  def __repr__(self):
+    return "Created student: %s" % self.__str__()
+
+  @property
+  def student_dict(self):
+    return { attr : getattr(self, attr) for attr in self.indexes }
