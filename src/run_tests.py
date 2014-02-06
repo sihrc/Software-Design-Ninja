6d878e662d9cf0d94bbc6ff3ddbf205098791d36
Chris <sihrc.c.lee@gmail.com>
2014-02-06 15:43:31 -0500

    Added students and test class in run_tests

--- src/run_tests.py
+++ src/run_tests.py
@@ -2,6 +2,18 @@ import get_repos as git
 import roster
 import tests
 
+class Tester:
+	def __init__(self, cases = dict()):
+		self.cases = cases
+
+	def addArgs (self, result, **args):
+		return **args
+
+
 if __name__ == "__main__":
-	students =  roster.groupD
-	
+	students = roster.groupD
+	#git.clone_repos(students.values()) # Initial Run Only
+	git.pull_repos(students.values())
+
+	for student in students:
+		exec("import repos." + students.keys())
