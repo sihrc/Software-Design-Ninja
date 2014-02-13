82ee95d6550bd09ec3dc484700782d886c9b7fd5
Chris <sihrc.c.lee@gmail.com>
2014-02-13 16:18:52 -0500

    Only have my own students now

--- src/roster.py
+++ src/roster.py
@@ -1,4 +1,1 @@
-groupD = {"HWilk":"HannahWilk","ssingal05":"SiddharthSingal","jwoo1123":"JamesWoo","griffint":"GriffinTschurwald","InseongJoe":"InseongJoe","aloverso":"AnneLoVerso","RRameshwar":"RaaginiRameshwar","flymperopoulos":"FilippsLymperopoulos","runnersaw":"SawyerVaughan","lvanderlyn":"LindseyVanderlyn","shrinidhit":"ShrinidhiThirumalai","segerphilip":"PhilipSeger","emocallaghan":"ElizabethOCallaghan", "kdy304g":"DannyKang"}
-missing = ["DannyKang","DuncanMichael"]
-
-groupA = {"cbauerswald":"CeceliaAuerswald", "mafaldaborges":"MafaldaBorges", "dcelik":"DenizCelik", "rileychapman":"RileyChapman", "dennis-chen":"DennisChen", "yunhsincynthiachen":"CynthiaChen", "logandavis":"LoganDavis", "":"ZoherGhadyali", "dinopants174":"SusieGrimshaw", "sgrim3":"SubhashGubba", "iangmhill":"IanHill", "mikaiw":"MikaIchikiWelches", "PMKeene":"MaireKeene", "doyunglee":"DoyungLee"}
+groupD = {"InseongJoe":"InseongJoe", "aloverso":"AnneLoVerso", "RRameshwar":"RaaginiRameshwar", "flymperopoulos":"FilippsLymperopoulos", "emocallaghan":"ElizabethOCallaghan",  "kdy304g":"DannyKang",  "mortier":"DuncanMichael"}
